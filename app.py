84523a266c62bc45ad324fc94c169b9ba713b89f
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2022-08-06 19:50:29 +0200

    Mostly works, still in testing

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 
 from flask import Flask, request
 from redis import Redis
-import json
 import random
 
 app = Flask("qpassword_manager")
@@ -11,34 +10,107 @@ redis = Redis(decode_responses=True)
 
 @app.route("/register", methods=["post"])
 def register():
+    taken_usernames = []
+    for username in filter(
+            lambda key: not key.isdigit(), redis.keys("*")):
+        taken_usernames.append(username)
+
+    for username in map(
+        lambda code: redis.json().get(code)[0],
+        filter(lambda key: key.isdigit(), redis.keys("*")),
+    ):
+        taken_usernames.append(username)
+
     username = request.json["username"]
+
+    if username in taken_usernames:
+        return "Username already taken"
+
+    if username[0].isdigit():
+        return "Invalid name: Your username can't start with a number"
+
     password = request.json["password"]
     email = request.json["email"]
 
-    tokens = redis.keys("*")
+    if redis.ttl("0emails") == -2:
+        redis.json().set("0emails", "$", [])
+
+    elif email in redis.json().get("0emails", "$")[0]:
+        return "Account with this email address is already registered"
+
+    keys = redis.keys("*")
     while True:
-        token = random.randint(100000, 999999)
-        if token not in tokens:
+        code = random.randint(100000, 999999)
+        if code not in keys:
             break
 
-    print(token)
-    redis.set(token, f'["{username}", "{password}", "{email}"]', 3600)
+    print(code)
+    redis.json().set(code, "$", [username, password, email], 3600)
+    return "Please confirm your email to finish the registration" + f": http://localhost:5000/confirm_email/{username}/{code}"
+
+
+@app.route("/confirm_email/<username>/<code>")
+def confirm_email(username, code):
+    try:
+        account = redis.json().get(code)
+
+        if account[0] == username:
+            redis.json().set(
+                f"{account[0]}",
+                "$",
+                {"master_key": account[1], "email": account[2],
+                    "next_id": 0, "passwords": {}}
+            )
+            redis.delete(code)
+            redis.json().arrappend("0emails", "$", account[2])
+            return "Successfully verified"
+
+    except TypeError:
+        print(f"Code {code} not in database")
+
+    return "Invalid/Expired code"
+
+
+@app.route("/check_credentials", methods=["post"])
+def check_credentials():
+    if redis.ttl(request.authorization["username"]) != -2:
+        user = redis.json().get(
+            request.authorization["username"])
+        if user["master_key"] == request.authorization["password"]:
+            return "1"
+
     return ""
-
-
-@app.route('/confirm_email/<token>')
-def confirm_email(token):
-    account = json.loads(redis.get(token))
-
-    redis.json().set(f'{account[0]}', '$', f'{{ \"master_key\": \"{account[1]}\", \"email\": \"{account[2]}\", \"passwords\": [] }}')
-    return token
 
 
 @app.route("/add_to_database", methods=["post"])
 def add_to_database():
     if request.authorization:
-        print(redis.keys("*"))
-    return "hi"
+        if check_credentials():
+            username = request.authorization["username"]
+            entry_id = redis.json().get(username, "$.next_id")[0]
+            redis.json().set(
+                username, f"$.passwords.{entry_id}", request.json)
+            redis.json().numincrby(username, "$.next_id", 1)
+
+    return ""
 
 
-app.run(debug=True)
+@app.route("/remove_from_database", methods=["post"])
+def remove_from_database():
+    if request.authorization:
+        if check_credentials():
+            username = request.authorization["username"]
+            entry_id = request.json["id"]
+            entry = redis.json().get(username, f"$.passwords.{entry_id}")
+            if entry:
+                redis.json().set(username, f"$.passwords._{entry_id}", entry[0])
+                redis.json().delete(username, f"$.passwords.{entry_id}")
+
+            else:
+                print("Entry not in database")
+
+    return ""
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
