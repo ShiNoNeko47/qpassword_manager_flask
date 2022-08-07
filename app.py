1b9e06b76018a033a9b62135480f574267305bcf
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2022-08-07 22:09:51 +0200

    Working prototype

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ redis = Redis(decode_responses=True)
 @app.route("/register", methods=["post"])
 def register():
     taken_usernames = []
-    for username in filter(
-            lambda key: not key.isdigit(), redis.keys("*")):
+    for username in filter(lambda key: not key.isdigit(), redis.keys("*")):
         taken_usernames.append(username)
 
     for username in map(
@@ -46,7 +45,10 @@ def register():
 
     print(code)
     redis.json().set(code, "$", [username, password, email], 3600)
-    return "Please confirm your email to finish the registration" + f": http://localhost:5000/confirm_email/{username}/{code}"
+    return (
+        "Please confirm your email to finish the registration"
+        + f": http://localhost:5000/confirm_email/{username}/{code}"
+    )
 
 
 @app.route("/confirm_email/<username>/<code>")
@@ -58,8 +60,12 @@ def confirm_email(username, code):
             redis.json().set(
                 f"{account[0]}",
                 "$",
-                {"master_key": account[1], "email": account[2],
-                    "next_id": 0, "passwords": {}}
+                {
+                    "master_key": account[1],
+                    "email": account[2],
+                    "next_id": 0,
+                    "passwords": {},
+                },
             )
             redis.delete(code)
             redis.json().arrappend("0emails", "$", account[2])
@@ -74,8 +80,7 @@ def confirm_email(username, code):
 @app.route("/check_credentials", methods=["post"])
 def check_credentials():
     if redis.ttl(request.authorization["username"]) != -2:
-        user = redis.json().get(
-            request.authorization["username"])
+        user = redis.json().get(request.authorization["username"])
         if user["master_key"] == request.authorization["password"]:
             return "1"
 
@@ -89,7 +94,10 @@ def add_to_database():
             username = request.authorization["username"]
             entry_id = redis.json().get(username, "$.next_id")[0]
             redis.json().set(
-                username, f"$.passwords.{entry_id}", request.json)
+                username,
+                f"$.passwords.{entry_id}",
+                [request.json[key] for key in ["website", "username", "password"]],
+            )
             redis.json().numincrby(username, "$.next_id", 1)
 
     return ""
@@ -106,8 +114,46 @@ def remove_from_database():
                 redis.json().set(username, f"$.passwords._{entry_id}", entry[0])
                 redis.json().delete(username, f"$.passwords.{entry_id}")
 
-            else:
-                print("Entry not in database")
+    return ""
+
+
+@app.route("/get_entry_ids", methods=["post"])
+def get_entry_ids():
+    if request.authorization:
+        if check_credentials():
+            username = request.authorization["username"]
+            objkeys = redis.json().objkeys(username, "$.passwords")[0]
+            entry_ids = [key for key in filter(lambda x: x.isdigit(), objkeys)]
+            return entry_ids
+
+    return ""
+
+
+@app.route("/get_entry", methods=["post"])
+def get_entry():
+    if request.authorization:
+        if check_credentials():
+            username = request.authorization["username"]
+            entry_id = request.json["id"]
+            entry = redis.json().get(username, f"$.passwords.{entry_id}")[0]
+            return entry
+
+    return ""
+
+
+@app.route("/get_all", methods=["post"])
+def get_all():
+    if request.authorization:
+        if check_credentials():
+            username = request.authorization["username"]
+            objkeys = redis.json().objkeys(username, "$.passwords")[0]
+            entry_ids = [key for key in filter(lambda x: x.isdigit(), objkeys)]
+            print(entry_ids)
+            entries = []
+            for entry_id in entry_ids:
+                entries.append(redis.json().get(username, f"$.passwords.{entry_id}")[0])
+
+            return entries
 
     return ""
 
