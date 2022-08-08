244a3f0411830324623a037c68dd1b0ce5a9d121
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2022-08-08 22:09:59 +0200

    Added needs_authorization decorator

--- app.py
+++ app.py
@@ -3,9 +3,21 @@
 from flask import Flask, request
 from redis import Redis
 import random
+from functools import wraps
 
 app = Flask("qpassword_manager")
 redis = Redis(decode_responses=True)
+
+
+def needs_authorization(func):
+
+    @wraps(func)
+    def wrapper():
+        if request.authorization:
+            if check_credentials():
+                return func()
+
+    return wrapper
 
 
 @app.route("/register", methods=["post"])
@@ -88,74 +100,63 @@ def check_credentials():
 
 
 @app.route("/add_to_database", methods=["post"])
+@needs_authorization
 def add_to_database():
-    if request.authorization:
-        if check_credentials():
-            username = request.authorization["username"]
-            entry_id = redis.json().get(username, "$.next_id")[0]
-            redis.json().set(
-                username,
-                f"$.passwords.{entry_id}",
-                [request.json[key] for key in ["website", "username", "password"]],
-            )
-            redis.json().numincrby(username, "$.next_id", 1)
-
-    return ""
+    username = request.authorization["username"]
+    entry_id = redis.json().get(username, "$.next_id")[0]
+    redis.json().set(
+        username,
+        f"$.passwords.{entry_id}",
+        [request.json[key]
+            for key in ["website", "username", "password"]],
+    )
+    redis.json().numincrby(username, "$.next_id", 1)
 
 
 @app.route("/remove_from_database", methods=["post"])
+@needs_authorization
 def remove_from_database():
-    if request.authorization:
-        if check_credentials():
-            username = request.authorization["username"]
-            entry_id = request.json["id"]
-            entry = redis.json().get(username, f"$.passwords.{entry_id}")
-            if entry:
-                redis.json().set(username, f"$.passwords._{entry_id}", entry[0])
-                redis.json().delete(username, f"$.passwords.{entry_id}")
-
-    return ""
+    username = request.authorization["username"]
+    entry_id = request.json["id"]
+    entry = redis.json().get(username, f"$.passwords.{entry_id}")
+    if entry:
+        redis.json().set(username, f"$.passwords._{entry_id}", entry[0])
+        redis.json().delete(username, f"$.passwords.{entry_id}")
 
 
 @app.route("/get_entry_ids", methods=["post"])
+@needs_authorization
 def get_entry_ids():
-    if request.authorization:
-        if check_credentials():
-            username = request.authorization["username"]
-            objkeys = redis.json().objkeys(username, "$.passwords")[0]
-            entry_ids = [key for key in filter(lambda x: x.isdigit(), objkeys)]
-            return entry_ids
-
-    return ""
+    username = request.authorization["username"]
+    objkeys = redis.json().objkeys(username, "$.passwords")[0]
+    entry_ids = [key for key in filter(lambda x: x.isdigit(), objkeys)]
+    print(entry_ids)
+    return entry_ids
 
 
 @app.route("/get_entry", methods=["post"])
+@needs_authorization
 def get_entry():
-    if request.authorization:
-        if check_credentials():
-            username = request.authorization["username"]
-            entry_id = request.json["id"]
-            entry = redis.json().get(username, f"$.passwords.{entry_id}")[0]
-            return entry
-
-    return ""
+    username = request.authorization["username"]
+    entry_id = request.json["id"]
+    entry = redis.json().get(username, f"$.passwords.{entry_id}")[0]
+    print(entry)
+    return entry
 
 
 @app.route("/get_all", methods=["post"])
+@needs_authorization
 def get_all():
-    if request.authorization:
-        if check_credentials():
-            username = request.authorization["username"]
-            objkeys = redis.json().objkeys(username, "$.passwords")[0]
-            entry_ids = [key for key in filter(lambda x: x.isdigit(), objkeys)]
-            print(entry_ids)
-            entries = []
-            for entry_id in entry_ids:
-                entries.append(redis.json().get(username, f"$.passwords.{entry_id}")[0])
+    username = request.authorization["username"]
+    objkeys = redis.json().objkeys(username, "$.passwords")[0]
+    entry_ids = [key for key in filter(lambda x: x.isdigit(), objkeys)]
+    entries = []
+    for entry_id in entry_ids:
+        entries.append(redis.json().get(
+            username, f"$.passwords.{entry_id}")[0])
+    print(entries)
 
-            return entries
-
-    return ""
+    return entries
 
 
 if __name__ == "__main__":
